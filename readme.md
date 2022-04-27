0de21fce92282063a65f9c90eee182fd2f3166b4
A <razaandry@hotmail.com>
2022-04-27 13:34:37 +0400

    interval item autoclick

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 Décommentez <br/>
 //let intervalCoreGame = setInterval(checkScore,10); <br/>
 //let autoclick = new Item("Autoclick", 1,  50, 1, false);
+
+
+à faire les setintervals des items
--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 /* Init */
 window.onload = initRender;
 
-let score = 40;
+let score = 0;
 let level = 1;
 
-let autoclick = new Item("Autoclick", 1,  50, 1, true);
-//let autoclick = new Item("Autoclick", 1,  50, 1, false);
-let chinois = new Item("Chinois", 100,  300, 1, false);
-let indien = new Item("Indien", 1000, 3000, 1, false);
+// let autoclick = new Item("Autoclick", 1,  50, 1, true);
+let autoclick = new Item("Autoclick", 1,  50, 0, false,1);
+let chinois = new Item("Chinois", 100,  300, 0, false,100);
+let indien = new Item("Indien", 1000, 3000, 0, false,1000);
 
 let arrItems = [autoclick, chinois, indien];
 
 
-//let intervalCoreGameInterface = setInterval(checkScore,10)
+let intervalCoreGameInterface = setInterval(checkScore,10)
 
 /* DOM */
 
@@ -24,7 +24,7 @@ let listitemDOM = document.getElementById('list-item');
 
 samoussaDOM.onclick = addSamoussas;
 
-/* Functions */
+/* Functions Interface Render*/
 function initRender() {
     renderScore();
     renderInterfaceItems();
@@ -53,14 +53,17 @@ function renderInterfaceItems() {
 
 }
 
-function Item(name, count, price, level, unlock) {
+/* Construct */
+function Item(name, count, price, level, unlock,scale) {
     this.name = name;
     this.count = count;
     this.price = price;
     this.level = level;
     this.unlock = unlock;
+    this.scale= scale;
 }
 
+/* Functions Game */
 function addSamoussas() {
     score++;
     renderScore();
@@ -69,14 +72,16 @@ function addSamoussas() {
 function upLevel(item){
 
     let item_ = arrItems.find(e=> e.name == item.id)
-    if(score >= (item_.price * item_.level))
+    if(score >= item_.price)
     {
-        score -= item_.price*item_.level;   //On déduit le nbre de samoussas de l'achat du niveau
+        score -= item_.price;   //On déduit le nbre de samoussas de l'achat du niveau
         renderScore();  //On le réaffiche
         item_.level ++; //On up l'item acheté
         item.innerText = "Niveau " +item_.level; //On met à jour le rendu btn niveau
-        item_.price *= item_.level; //On met à jour le nouveau prix 
+        item_.price *= (item_.level+1); //On met à jour le nouveau prix 
+        item_.count = item_.level == 1 ? item_.count : item_.count += item_.scale; 
         document.getElementById('price-'+item_.name).innerText = item_.price + " samoussas"; //On met à jour le rendu du prix
+        workingitems(item_)
     }
 
 }
@@ -84,8 +89,19 @@ function upLevel(item){
 function checkScore(){
     if(score >= 20 && !autoclick.unlock){
         autoclick.unlock = true;
-        btnAutoclick = document.getElementById('btn-Autoclick'); 
         renderInterfaceItems();
     }
+}
+
+function workingitems(item){
+    switch(item.name){
+        case "Autoclick":
+            let intervalAC = setInterval(()=>{score += autoclick.count,renderScore()},1000);
+            break;
+        default:
+            console.log('err working item autoclick');
+            break;
+    }
+
 }
 
